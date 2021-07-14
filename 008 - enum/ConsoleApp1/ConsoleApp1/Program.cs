34274f5a5db9ed002200a679c7c2254f43287880
BryanDietrichBernhardt <bryan_d_bernhardt@terra.com.br>
2021-07-14 08:26:29 -0300

    done enum activity

---
+++ 008 - enum/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ConsoleApp1.Entities;
+using ConsoleApp1.Entities.Enums;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+            OrderStatus os = (OrderStatus)Enum.Parse(typeof(OrderStatus), "Delivered");
+
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+        }
+    }
+}
