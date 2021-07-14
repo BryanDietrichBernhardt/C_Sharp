5a0e755e4f081b6ffc10a984023e56922a1b8228
BryanDietrichBernhardt <bryan_d_bernhardt@terra.com.br>
2021-07-14 09:32:06 -0300

    created entities

---
+++ 009 - composicao/ExercicioComposicao/ExercicioComposicao/Entities/HourContract.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioComposicao.Entities
+{
+    class HourContract
+    {
+        public DateTime date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+        public double TotalValue()
+        {
+            return Hours * ValuePerHour;
+        }
+    }
+}
---
+++ 009 - composicao/ExercicioComposicao/ExercicioComposicao/Entities/Worker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ExercicioComposicao.Entities.Enums;
+using ExercicioComposicao.Entities;
+
+namespace ExercicioComposicao.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+
+        public void AddContract(HourContract contract)
+        {
+
+        }
+        public void RemoveContract(HourContract contract)
+        {
+
+        }
+        public double Income(int year, int mouth)
+        {
+            return 2.0;
+        }
+    }
+}
