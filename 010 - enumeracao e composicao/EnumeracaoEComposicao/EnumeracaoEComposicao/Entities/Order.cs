8c94cabb1027374268565887a1978ae386c3e064
BryanDietrichBernhardt <bryan_d_bernhardt@terra.com.br>
2021-07-20 23:41:48 -0300

    Created Entities and Enums

---
+++ 010 - enumeracao e composicao/EnumeracaoEComposicao/EnumeracaoEComposicao/Entities/Order.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EnumeracaoEComposicao.Entities.Enums;
+
+namespace EnumeracaoEComposicao.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> Itens { get; set; }
+        public Client Client { get; set; }
+
+        public Order(OrderStatus status, Client client)
+        {
+            Moment = DateTime.Now;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Itens.Add(item);
+        }
+        public void RemoveItem(OrderItem item)
+        {
+            Itens.Remove(item);
+        }
+        public double Total()
+        {
+            double total = 0.0;
+            foreach(OrderItem item in Itens)
+            {
+                total += item.Product.Price;
+            }
+            return total;
+        }
+    }
+}
---
+++ 010 - enumeracao e composicao/EnumeracaoEComposicao/EnumeracaoEComposicao/Entities/OrderItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EnumeracaoEComposicao.Entities;
+
+namespace EnumeracaoEComposicao.Entities
+{
+    class OrderItem
+    {
+        public int Quantity { get; set; }
+        public Product Product { get; set; }
+
+        public OrderItem(int quantity, Product product)
+        {
+            Quantity = quantity;
+            Product = product;
+        }
+
+        public double SubTotal()
+        {
+            return Quantity * Product.Price;
+        }
+    }
+}
